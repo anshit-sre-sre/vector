44d3248790442daf22fde0d9848770fce40500d7
Pablo Sichert <mail@pablosichert.com>
2022-06-19 21:41:52 +0200

    Implement `Expression::resolve_batch` interface for VRL
    Signed-off-by: Pablo Sichert <mail@pablosichert.com>

--- lib/vrl/compiler/src/expression/if_statement.rs
+++ lib/vrl/compiler/src/expression/if_statement.rs
@@ -6,7 +6,7 @@ use crate::{
     expression::{Block, Predicate, Resolved},
     state::{ExternalEnv, LocalEnv},
     value::VrlValueConvert,
-    Context, Expression, TypeDef,
+    BatchContext, Context, Expression, TypeDef,
 };
 
 #[derive(Debug, Clone, PartialEq)]
@@ -18,7 +18,12 @@ pub struct IfStatement {
 
 impl Expression for IfStatement {
     fn resolve(&self, ctx: &mut Context) -> Resolved {
-        let predicate = self.predicate.resolve(ctx)?.try_boolean()?;
+        let predicate = self
+            .predicate
+            .resolve(ctx)
+            .expect("predicate must not be fallible")
+            .try_boolean()
+            .expect("predicate must be boolean");
 
         match predicate {
             true => self.consequent.resolve(ctx),
@@ -28,6 +33,43 @@ impl Expression for IfStatement {
                 .map(|block| block.resolve(ctx))
                 .unwrap_or(Ok(Value::Null)),
         }
+    }
+
+    fn resolve_batch(&self, ctx: &mut BatchContext) {
+        self.predicate.resolve_batch(ctx);
+        let mut predicates = {
+            let mut moved = vec![Ok(Value::Null); ctx.len()];
+            std::mem::swap(ctx.resolved_values_mut(), &mut moved);
+            moved
+        };
+
+        let mut i = 0;
+        let mut ctx_false = ctx.drain_filter(|_| {
+            let predicate = {
+                let mut moved = Ok(Value::Null);
+                std::mem::swap(&mut predicates[i], &mut moved);
+                moved
+            };
+            let predicate = predicate
+                .expect("predicate must not be fallible")
+                .try_boolean()
+                .expect("predicate must be boolean");
+
+            i += 1;
+
+            !predicate
+        });
+
+        self.consequent.resolve_batch(ctx);
+        if let Some(alternative) = &self.alternative {
+            alternative.resolve_batch(&mut ctx_false);
+        } else {
+            for resolved in ctx_false.resolved_values_mut() {
+                *resolved = Ok(Value::Null);
+            }
+        }
+
+        ctx.extend(ctx_false);
     }
 
     fn type_def(&self, state: (&LocalEnv, &ExternalEnv)) -> TypeDef {
